5a085c6ec4ef42c4fd0f6084296cf9011d9c7071
Mark Richardson <mark_richardson@sky.com>
2020-11-20 16:55:24 +0000

    Classes set for player and game

--- app/models/rps_game.py
+++ app/models/rps_game.py
@@ -1,4 +1,5 @@
-from app.models.rps_player import *
+class Game():
 
-
-
+    def __init_(self, player1_choice, player2_choice, winner):
+        self.player1_choice = player1_choice
+        self.player2_choice = player2_choice
